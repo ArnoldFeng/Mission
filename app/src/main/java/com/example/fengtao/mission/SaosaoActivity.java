eb1e2221f9dc41ee07005e186118c57cb232529c
ArnoldFeng <fengtaogogogo@163.com>
2018-09-04 17:19:09 +0800

    二维码扫描功能实现

---
+++ app/src/main/java/com/example/fengtao/mission/SaosaoActivity.java
@@ -0,0 +1,11 @@
+package com.example.fengtao.mission;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SaosaoActivity extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceStated){
+        super.onCreate(savedInstanceStated);
+        setContentView(R.layout.activity_saosao);
+    }
+}
